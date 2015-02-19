261744bd631246849f0265c6be9bce6762963ae6
Paul Hadfield <paul@phad.org.uk>
2015-02-19 23:42:07 +0000

    Add fingerprint for '8'.

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -11,7 +11,7 @@ KNOWN_CHARS = {
   'unknown5' => '5',
   'ZGIxOGVlOWI0MWY0NzFlNDQ3YWQyMDI2OGIzNmFjN2IxNGI2OGI1OQ==' => '6',
   'unknown7' => '7',
-  'unknown8' => '8',
+  'MzI5Mjc4MGRiNzZmYmIzY2E5NWM0MzQ3NzVhYjI4MGE5ZTIwOWUzNQ==' => '8',
   'OTYxYjg1ZjVmZTFhYTVlOTUxM2VmNDRhOTg3NTBkNGJkNjNiYjNlNg==' => '9',
   'MDI1YmZmYzg2YzY1ZTc3ODRiOTMwMGY4NGY1ZDc0Mjg5YTI2NDE0Zg==' => '.',
   'MmMzYWVlNTgxZTk3MGM4Y2NmZWU4MTJjY2Q5OGM5ODcxZDE2MzdkYQ==' => 'G',
@@ -27,7 +27,7 @@ quota = splits.each_with_index.map do |j, idx|
   fp = j.fingerprint.strip
   c = KNOWN_CHARS[fp]
   if c.nil?
-    puts "Unrecognise char with fingerprint: #{fp}"
+    puts "Unrecognized char with fingerprint: #{fp}"
     j.write("image-#{fp}.pgm")
   end
   c
